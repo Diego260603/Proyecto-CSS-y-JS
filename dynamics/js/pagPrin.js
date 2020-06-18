81808e56ffdf8d9b396d13c078fa327221bafd7c
Diego260603 <yinga100@gmail.com>
2020-06-17 20:20:19 -0500

    FIX: Stetics changes and Modals Added

---
+++ dynamics/js/pagPrin.js
@@ -0,0 +1,24 @@
+/*var expiresdate = new Date(2021, 1, 02, 11, 20);
+var cookievalue = "Hola Mundo!";
+function crearCookie(){
+  document.cookie = "comida_favorita=arroz; max-age=3600; path=/";
+  document.cookie = "color_favorito=amarillo";
+
+}*/
+document.cookie = “miCookie=Soy la cookie”;
+//document.cookie = "nombre="cookievalue + "; expires=" + expiresdate.toUTCString();
+
+
+/*
+function guardarCookie(nombre,valor,fecha)
+{
+  document.cookie = nombre+"="+valor+";expires="+fecha;
+
+}
+var caduca = "31 Dec 2020 23:59:59 GMT"
+function guardar(){
+  tuCookie = "pruebaCookie"
+  tuValor = document.dato.miCookie.value
+  guardarCookie(tuCookie,tuValor,caduca)
+}
+		*/
