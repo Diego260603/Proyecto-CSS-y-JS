efbb5d306b7960308d3ad6a2f721cd3ffe88792d
Diego260603 <yinga100@gmail.com>
2020-06-19 01:24:51 -0500

    FIX: All html codes comented

--- dynamics/js/pagScores.js
+++ dynamics/js/pagScores.js
@@ -1,2 +1,10 @@
 //Este archivo se llama pagScores.js
 //Está agregado en la parte de abajo de la página SCORES.html
+
+var la = JSON.parse($.cookie("LAhighscores"));
+var te = JSON.parse($.cookie("TEhighscores"));
+var si = JSON.parse($.cookie("SIhighscores"));
+
+function tablificar(codigo,array){
+
+}
